801a6e8c8a576bbece978ab5e085cc6ce4b9440b
Kyle Kirkland <kyle.kirkland0528@me.com>
2016-09-07 20:01:46 -0400

    Initial Commit

---
+++ NetworkOperation/NetworkOperation.h
@@ -0,0 +1,19 @@
+//
+//  NetworkOperation.h
+//  NetworkOperation
+//
+//  Created by Kyle Kirkland on 9/7/16.
+//  Copyright © 2016 Kirkland Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkOperation.
+FOUNDATION_EXPORT double NetworkOperationVersionNumber;
+
+//! Project version string for NetworkOperation.
+FOUNDATION_EXPORT const unsigned char NetworkOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkOperation/PublicHeader.h>
+
+
